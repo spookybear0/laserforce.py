653df39c35ab33d8d02da0a67effeba85ae9c45a
Spookybear0 <collinmcarroll@gmail.com>
2020-05-07 15:42:24 -0700

    fix

--- laserforce.py
+++ laserforce.py
@@ -1,4 +1,12 @@
 import requests
+class Missions:
+    def request(self, id):
+        id = str(id).replace("-", " ", 2)
+        id = id.split()
+        PARAMS = {"requestId": "1" ,"regionId": "9999","siteId": "9999", "memberRegion": id[0], "memberSite": id[1],"memberId": id[2], "token": ""}
+        r = requests.post(url = "http://v2.iplaylaserforce.com/recentMissions.php", data=PARAMS)
+        r = r.json()
+        r = r["mission"]
 class Stats:
     def request(self, id):
         id = str(id).replace("-", " ", 2)
@@ -27,4 +35,3 @@ class Stats:
     def skillLevelName(self, id):
         r = self.request(id)
         return r["skillLevelName"]
-Stats().request("4-43-703")
