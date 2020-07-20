49f2dc25dcfdf0928f08f8f75770e665fc7537fa
Spookybear0 <collinmcarroll@gmail.com>
2020-07-19 17:37:17 -0700

    fix

---
+++ laserforce/other.py
@@ -0,0 +1,10 @@
+from enums import Enum
+
+class Levels(Enum):
+    one = "Recruit"
+    two = "Gunner"
+    three = "Trooper"
+    four = "Captain"
+    five = "Starlord"
+    six = "Lasermaster"
+    
--- laserforce/summary.py
+++ laserforce/summary.py
@@ -1,3 +1,4 @@
+import laserforce
 class Summary:
     """Class that represents a laserforce account's mission Summary. This class gives you a mission summary of a member. You can also initalize this class with kwargs."""
     def __init__(self, content, **kw):
@@ -8,16 +9,26 @@ class Summary:
             raise ValueError
         content = content["summary"]
         self.content = content
-        try: self.standard = kw["standard"]
-        except: pass
-        try: self.other = kw["other"]
-        except: pass
-        try: self.counter_strike = kw["counter_strike"]
-        except: pass
-        try: self.space_marines = kw["space_marines"]
-        except: pass
-        try: self.ctf = kw["ctf"]
-        except: pass
+        try: 
+            self.standard = kw["standard"]
+        except KeyError:
+            pass
+        try:
+            self.other = kw["other"]
+        except KeyError: 
+            pass
+        try: 
+            self.counter_strike = kw["counter_strike"]
+        except KeyError:
+            pass
+        try:
+            self.space_marines = kw["space_marines"]
+        except KeyError:
+            pass
+        try: 
+            self.ctf = kw["ctf"]
+        except KeyError: 
+            pass
 
     @property
     def standard(self):
@@ -43,4 +54,4 @@ class Summary:
     def ctf(self):
         """Returns list [gameType, #missions, dateLastPlayed, highScore, averageScore]"""
         return self.content[4]
-        
+    
