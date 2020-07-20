49f5d45984ba56107056f79bb827ff61c19b6480
Spookybear0 <collinmcarroll@gmail.com>
2020-07-19 17:24:36 -0700

    Update __init__.py

--- laserforce/__init__.py
+++ laserforce/__init__.py
@@ -1,6 +1,2 @@
-from laserforce.main import *
-from laserforce.achievements import *
-from laserforce.leaderboard import *
-from laserforce.missions import *
-from laserforce.stats import *
-from laserforce.summary import *
+from laserforce.main import get_stats, get_achievements, get_missions, get_summary, get_leaderboard, get_leaderboard_from_id
+from laserforce.enums import *
