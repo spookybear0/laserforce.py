74d70dc5411907587759b6e379902d8ddc618ace
Spookybear0 <collinmcarroll@gmail.com>
2020-07-19 19:24:00 -0700

    tox

--- setup.py
+++ setup.py
@@ -1,14 +1,11 @@
 from setuptools import setup
 
-setup(name='laserforce',
-      version='1.0.0',
+setup(name="laserforce",
+      version="1.0.0",
       description="Python package for laserforce.",
       long_description="",
-      author='SpookyBear0',
-      author_email='collinmcarroll@gmail.com',
-      packages=['laserforce'],
+      author="SpookyBear0",
+      author_email="collinmcarroll@gmail.com",
+      packages=["laserforce"],
       zip_safe=False,
-      install_requires=[
-          requests
-          ],
       )
