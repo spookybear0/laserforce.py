a117813f70d0ec43dec7fec2db10e10171d0f9c0
Spookybear0 <collinmcarroll@gmail.com>
2020-07-22 02:52:27 -0700

    Update setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name="laserforce.py",
+      version="1.0.01",
+      description="Python package for laserforce.",
+      long_description="",
+      author="SpookyBear0",
+      author_email="collinmcarroll@gmail.com",
+      packages=["laserforce"],
+      zip_safe=False,
+      )
