279ae420b3775a5065b96c9830ebc7a963328522
SpookyBear0 <50897638+SpookyBear0@users.noreply.github.com>
2020-07-22 02:56:58 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name="laserforce.py",
-      version="1.0.01",
+      version="1.0.0",
       description="Python package for laserforce.",
       long_description=(root / "README.rst").read_text("utf-8"),
       author="SpookyBear0",
